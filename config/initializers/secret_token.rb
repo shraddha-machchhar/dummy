6cba6f5f7eae6d372dfe2874dea7a4b73158d4eb
nikunj.0407@gmail.com <nikunj.0407@gmail.com>
2013-01-09 17:06:52 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '695d0e302df0281fa2cd55bf3e3fb2d78028f1ab8f47f58bbe95fc8f5aa0fb9528885c331db3ccbeea246b345080542d0a9685193f04b2a977aeb39cd7b95d8c'
